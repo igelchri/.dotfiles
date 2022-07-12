7e0b25f1865647415ff7fe8d7e1afbf5f2d0e02b
igelchri <phonetisch@arcor.de>
2022-07-12 23:10:14 +0200

    neuer bspwm ordner

---
+++ bspwm/.config/bspwm/autostart.sh
@@ -0,0 +1,48 @@
+#!/bin/bash
+###--------  Meine BSPWMRC - Autostart  ----------
+###--------  Stand: 220404 ---------
+
+## Autostart -------------------------------------------------#
+
+# Kill if already running
+killall -9 xsettingsd sxhkd dunst ksuperkey xfce4-power-manager
+
+# Lauch xsettingsd daemon
+xsettingsd &
+
+# polkit agent
+if [[ ! `pidof xfce-polkit` ]]; then
+	/usr/lib/xfce-polkit/xfce-polkit &
+fi
+
+# Lauch keybindings daemon
+#sxhkd &
+sxhkd -c ~/.config/bspwm/sxhkd/sxhkdrc &
+
+# Enable Super Keys For Menu
+ksuperkey -e 'Super_L=Alt_L|F1' &
+ksuperkey -e 'Super_R=Alt_L|F1' &
+
+# Enable power management
+xfce4-power-manager &
+
+# Fix cursor
+xsetroot -cursor_name left_ptr
+
+# Restore wallpaper
+bash $HOME/.fehbg
+#bash $HOME/.config/bspwm/.fehbg
+
+# Start mpd
+exec mpd &
+
+#start emacs-dämon
+exec /usr/bin/emacs --daemon &
+
+# Start bspwm scripts
+bspcolors
+bspbar
+bspcomp
+bspdunst
+bspfloat &
+
